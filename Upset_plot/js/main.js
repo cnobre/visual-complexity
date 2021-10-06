34af9e82ef2de091f57694dc36c42447dd6fb58f
Carolina <carolinanobre@gmail.com>
2021-10-06 01:21:31 -0400

    added active training for upset

--- Upset_plot/js/main.js
+++ Upset_plot/js/main.js
@@ -2,9 +2,33 @@
 d3.json('data/dataset.json')
     .then(data => {
         UpSetPlot(data);
-        // startTour();
+        // setUp();
+        startTour();
         // animate();
     });
+
+
+const containerWidth = 1000;
+const containerHeight = 350;
+
+const margin = { top: 100, right: 0, bottom: 0, left: 0 };
+const innerMargin = 12;
+const tooltipMargin = 10;
+
+const width = containerWidth - margin.left - margin.right;
+const height = containerHeight - margin.top - margin.left;
+
+padding = 20;
+const leftColWidth = 250;
+const mainChartWidth = 600;
+const rightColWidth = width - leftColWidth - mainChartWidth;
+
+console.log('leftColWidth', leftColWidth)
+console.log('MainChartWidth', mainChartWidth)
+console.log('rightColWidth', rightColWidth)
+
+const topRowHeight = 130;
+const bottomRowHeight = height - topRowHeight - innerMargin;
 
 
 function UpSetPlot(data) {
@@ -23,29 +47,7 @@ function UpSetPlot(data) {
     });
 
 
-    const containerWidth = 1000;
-    const containerHeight = 350;
 
-    const margin = { top: 100, right: 0, bottom: 0, left: 0 };
-    const innerMargin = 12;
-    const tooltipMargin = 10;
-
-    const width = containerWidth - margin.left - margin.right;
-    const height = containerHeight - margin.top - margin.left;
-
-    padding = 20;
-    const leftColWidth = 250;
-    const mainChartWidth = 600;
-    const rightColWidth = width - leftColWidth - mainChartWidth ;
-
-    console.log('leftColWidth',leftColWidth)
-    console.log('MainChartWidth',mainChartWidth)
-    console.log('rightColWidth',rightColWidth)
-
-    const topRowHeight = 130;
-    const bottomRowHeight = height - topRowHeight - innerMargin;
-
-    
 
     const IntersectionChartScale = d3.scaleLinear()
         .range([topRowHeight, 0])
@@ -56,12 +58,12 @@ function UpSetPlot(data) {
     //     .domain([0, d3.max(data.sets, (d) => d.size)]);
 
     const rightColScale = d3.scaleLinear()
-        .range([rightColWidth-padding, padding*2])
+        .range([rightColWidth - padding, padding * 2])
         .domain([0, d3.max(data.sets, (d) => d.size)]);
 
 
     const xScale = d3.scaleBand()
-        .range([padding, mainChartWidth-padding])
+        .range([0, mainChartWidth - padding])
         .domain(data.combinations.map((d) => d.combinationId))
         .paddingInner(0.2);
 
@@ -75,28 +77,21 @@ function UpSetPlot(data) {
         .attr('width', containerWidth)
         .attr('height', containerHeight)
         .append('g')
+        .attr('class', 'plot-container')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-     //pre-compute bar heights and positions for animation purposes; 
+    //pre-compute bar heights and positions for animation purposes; 
 
-   //labels     
+    //labels     
     const labels = svg.append('g')
-    .attr('class', 'labels')
-    .attr('transform', `translate(0, ${topRowHeight + innerMargin})`);
+        .attr('class', 'labels')
+        .attr('transform', `translate(0, ${topRowHeight + innerMargin})`);
 
 
-    //horizontal bars    
-    const hbars = svg.append('g')
-        .attr('class', 'horizontal-bars')
-        .attr('transform', `translate(${leftColWidth+mainChartWidth}, ${topRowHeight + innerMargin})`);
-
-    //vertical bars    
-    const vbars = svg.append('g')
-        .attr('class', 'vertical-bars')
-        .attr('transform', `translate(${leftColWidth}, 0)`);
 
     //circles    
     const combinationMatrix = svg.append('g')
+        .attr('class', 'setCircles')
         .attr('transform', `translate(${leftColWidth}, ${topRowHeight + innerMargin})`);
 
 
@@ -104,27 +99,47 @@ function UpSetPlot(data) {
         .data(data.combinations)
         .join('g')
         .attr('class', 'combination')
-        .attr('id', d=>d.combinationId)
-        .attr('transform', (d) => `translate(${xScale(d.combinationId) + xScale.bandwidth()/2.5}, 0)`)
+        .style('opacity', 0)
+        .attr('id', d => d.combinationId)
+        .attr('transform', (d) => `translate(${xScale(d.combinationId) + xScale.bandwidth() / 2.5}, 0)`)
 
 
     const circle = combinationGroup.selectAll('circle')
         .data((combination) => combination.sets)
         .join('circle')
         .classed('member', (d) => d.member)
-        .attr('cy', (d) => yScale(d.setId) + yScale.bandwidth()/2.5)
-        .attr('r', yScale.bandwidth()/2)
-    
+        .attr('cy', (d) => yScale(d.setId) + yScale.bandwidth() / 2.5)
+        .attr('r', yScale.bandwidth() / 2)
+
+    //horizontal bars    
+    const hbars = svg.append('g')
+        .attr('class', 'horizontal-bars')
+        // .attr('transform', `translate(${leftColWidth+mainChartWidth}, ${topRowHeight + innerMargin})`);
+        .attr('transform', `translate(${leftColWidth}, ${topRowHeight + innerMargin})`);
+
+    //vertical bars    
+    const vbars = svg.append('g')
+        .attr('class', 'vertical-bars')
+    // .attr('transform', `translate(${leftColWidth}, 0)`);
+
+
+    //group for whatever is currently being highlighted by the tour;
+    svg.append("g").attr("class", "tourGroup")
+        .append('rect') //background to create margin around selected elements
+        .style('opacity', .2)
+        .style('fill', 'red')
+
+
     hbars.selectAll('.bar')
         .data(data.sets)
         .join('rect')
         .attr('class', 'bar')
-        .attr('id',d=>d.setId + 'Bar')
+        .attr('id', d => d.setId + 'Bar')
         .attr('width', (d) => rightColWidth - rightColScale(d.size))
         .attr('height', yScale.bandwidth())
         .attr('x', 0)
         .attr('y', (d) => yScale(d.setId))
-        .on('mouseover', (event,d) => {
+        .on('mouseover', (event, d) => {
             d3.select('#tooltip')
                 .style('opacity', 1)
                 .html(d.label.join('<br/>'));
@@ -138,30 +153,30 @@ function UpSetPlot(data) {
             d3.select('#tooltip').style('opacity', 0);
         });
 
-        hbars.selectAll('.bar-label')
+    hbars.selectAll('.bar-label')
         .data(data.sets)
         .join('text')
         .attr('class', 'bar-label')
-        .attr('id',d=>d.setId + 'Label')
-        .style("font-family",'Arial')
+        .attr('id', d => d.setId + 'Label')
+        .style("font-family", 'Arial')
         .attr("fill", "#b3b1b1")
-        .attr('y', (d) => yScale(d.setId) + yScale.bandwidth()/2.5)
-        .attr('x',(d) => rightColWidth - rightColScale(d.size)+ 5)
+        .attr('y', (d) => yScale(d.setId) + yScale.bandwidth() / 2.5)
+        .attr('x', (d) => rightColWidth - rightColScale(d.size) + 5)
         // .attr('y', (d) => yScale(d.setId) + yScale.bandwidth()/2.5)
         .attr('dy', '0.35em')
-        .text((d) => d.size*100 + "%");
+        .text((d) => d.size * 100 + "%");
 
 
     labels.selectAll('.set-name')
         .data(data.sets)
         .join('text')
         .attr('class', 'set-name')
-        .attr('id',d=>d.setId + 'Label')
+        .attr('id', d => d.setId + 'Label')
         .attr('text-anchor', 'end')
         .attr('font-size', "14px")
-        .style("font-family",'Arial')
-        .attr('x', leftColWidth)
-        .attr('y', (d) => yScale(d.setId) + yScale.bandwidth()/2.5)
+
+        .attr('x', leftColWidth - padding)
+        .attr('y', (d) => yScale(d.setId) + yScale.bandwidth() / 2.5)
         .attr('dy', '0.35em')
         .text((d) => d.setId);
 
@@ -170,12 +185,13 @@ function UpSetPlot(data) {
         .data(data.combinations)
         .join('rect')
         .attr('class', 'bar')
-        .attr('id',d=>d.setMembership.reduce((acc,cValue)=>cValue + acc,'') + 'Bar')
+        .attr('id', d => d.setMembership.reduce((acc, cValue) => cValue + acc, '') + 'VBar')
         .attr('height', (d) => topRowHeight - IntersectionChartScale(d.value))
         .attr('width', xScale.bandwidth())
-        .attr('x', (d) => xScale(d.combinationId))
+        .attr('x', (d) => leftColWidth + xScale(d.combinationId))
         .attr('y', (d) => IntersectionChartScale(d.value))
-        .on('mouseover', (event,d) => {
+        .style('opacity', 0)
+        .on('mouseover', (event, d) => {
             d3.select('#tooltip')
                 .style('opacity', 1)
                 .html(d.labels.join('<br/>'));
@@ -188,18 +204,18 @@ function UpSetPlot(data) {
         .on('mouseout', () => {
             d3.select('#tooltip').style('opacity', 0);
         })
-        .on('click',function (event,d){
+        .on('click', function (event, d) {
             collapseCircles(this)
         })
 
 
-        // vbars.selectAll('.bar-label')
-        // .data(data.combinations)
-        // .join('text')
-        // .attr('class', 'bar-label')
-        // .attr('transform',d=>`translate(${xScale(d.combinationId)}, ${IntersectionChartScale(d.value)})rotate(-90)`)
-        // .attr('dy', xScale.bandwidth())
-        // .text((d) => d.value + "%");
+    // vbars.selectAll('.bar-label')
+    // .data(data.combinations)
+    // .join('text')
+    // .attr('class', 'bar-label')
+    // .attr('transform',d=>`translate(${xScale(d.combinationId)}, ${IntersectionChartScale(d.value)})rotate(-90)`)
+    // .attr('dy', xScale.bandwidth())
+    // .text((d) => d.value + "%");
 
 
     svg.append("text")
@@ -210,7 +226,7 @@ function UpSetPlot(data) {
         .style("font-size", "22px")
         .style("font-weight", 700)
         .style("text-decoration", "bold")
-        .style("font-family",'Arial')
+        .style("font-family", 'Arial')
         .text("Which symptoms do COVID patients have?");
 
     svg.append("text")
@@ -221,7 +237,7 @@ function UpSetPlot(data) {
         .style("font-size", "14px")
         .style("font-weight", 100)
         .style("text-decoration", "bold")
-        .style("font-family",'Arial')
+        .style("font-family", 'Arial')
         .text("The users of the COVID Symptoms tracker reported their symptoms.");
 
     svg.append("text")
@@ -232,35 +248,129 @@ function UpSetPlot(data) {
         .style("font-size", "14px")
         .style("font-weight", 100)
         .style("text-decoration", "bold")
-        .style("font-family",'Arial')
+        .style("font-family", 'Arial')
         .text("The infographic shows the frequency of each symptom and combination of symptoms.");
 
 }
 
-function startTour(){
+function startTour() {
 
-    steps.map(step=>{
+    // Add callbacks
+    //callback for when user clicks on cough bar
+    d3.select("#CoughBar").on("click", () => {
+        let currentStep = tour.getCurrentStep();
+        if (currentStep.id == 's2' && tour.isActive()) {
+            revealCol('#AnosmiaFatigueVBar', '#a')
+
+            // reveal1Col();
+
+        }
+    });
+
+    d3.select("#CoughVBar").on("click", () => {
+        let currentStep = tour.getCurrentStep();
+        if (currentStep.id == 's6' && tour.isActive()) {
+            tour.next()
+
+            // reveal1Col();
+
+        }
+    });
+    
+
+    // //move hbars and labels into group for the tour; 
+    // let tourGroup = d3.select(".tourGroup");
+
+    // tourGroup.node().append(document.querySelector(".horizontal-bars")); //move the icon into the group wrapper
+    // tourGroup.node().append(document.querySelector(".labels")); //move the icon into the group wrapper
+
+
+
+    steps.map(step => {
         tour.addStep(step);
     })
-tour.start();
+
+    // setTourRect()
+    tour.start();
 
 }
 
-function collapseCircles(element){
+function setTourRect() {
+    let tourGroup = d3.select(".tourGroup");
+
+    let tourGroupRect = tourGroup.node().getBoundingClientRect();
+    console.log(tourGroupRect)
+
+    tourGroup.select('rect')
+        .attr('x', tourGroupRect.x - 5)
+        .attr('y', tourGroupRect.y - 100)
+        .attr('width', tourGroupRect.width + 10)
+        .attr('height', tourGroupRect.height)
+
+}
+function reveal1Col() {
+
+    //move hbars and labels back outside of tourgroup; 
+    d3.select('.plot-container').node().append(document.querySelector(".horizontal-bars")); //move the icon into the group wrapper
+    // d3.select('.plot-container').node().append(document.querySelector(".labels")); //move the icon into the group wrapper
+
+    revealCol('#AnosmiaFatigueBar', '#a')
+
+
+}
+
+function revealCol(barId, circleId) {
+    // let tourGroup = d3.select(".tourGroup");
+
+    // //move first vbar into tourgroup; 
+    // tourGroup.node().append(document.querySelector(barId));
+    // //move the upset circles for the first vbar into tourgroup; 
+    // // tourGroup.node().append(document.querySelector("#a")); //move the icon into the group wrapper
+
+       //scope the animation selector to the group
+       let g = gsap.utils.selector(circleId);
+
+       //select all circles that are inside this group. 
+       let circles = g(".member");
+
+    gsap.timeline()
+        .to('.bar',{stroke:'',duration: 1 })
+        .to('.member', { stroke:'', duration: 1 }, "<")
+        .to('.horizontal-bars', { x: '+=20' },"<")
+        .to(barId, { opacity: 1, stroke:'#ffa282', strokeWidth:'4px',duration: 1 }, "<")
+        .to(circleId, { opacity: 1, duration: 1 }, "<")
+        .to(circles, { stroke:'rgb(255, 162, 130)',strokeWidth:'2px', duration: 1 }, "<")
+
+        .eventCallback("onComplete", () => tour.next())
+}
+
+function revealAllCols() {
+    gsap.timeline()
+        .to('.bar',{stroke:'',duration: 1 })
+        .to('.member', { stroke:'', duration: 1 }, "<")
+        .to('.horizontal-bars', { x: leftColWidth+mainChartWidth, duration:1 }, "<")
+        .to('.bar', { opacity: 1, duration: 1, stagger: 0.05}, "<")
+        .to('.combination', { opacity: 1, duration: 1, stagger: 0.05 }, "<")
+        .eventCallback("onComplete", () => tour.next())
+}
+
+
+
+function collapseCircles(element) {
 
     //isolate data bound to this rect
     let data = d3.select(element).data()[0];
     //use the data.combinationId to select the appropriate circle group
-    let group = d3.select('g #'+data.combinationId)
+    let group = d3.select('g #' + data.combinationId)
 
     //scope the animation selector to the group
     let g = gsap.utils.selector(group.node());
 
     //select all circles that are inside this group. 
-    let circles = g("circle"); 
+    let circles = g("circle");
 
     let tl = gsap.timeline(); //create the timeline
-    tl.to(circles, {opacity:1, cy: -20, duration: 1})
-    .to(circles, {opacity:1, cy: 10, duration: 1});
+    tl.to(circles, { opacity: 1, cy: -20, duration: 1 })
+        .to(circles, { opacity: 1, cy: 10, duration: 1 });
 
 }
--- Upset_plot/js/tour.js
+++ Upset_plot/js/tour.js
@@ -8,6 +8,8 @@
 //   }
 // });
 
+
+
 var tour = new Shepherd.Tour({
     defaultStepOptions: {
       //   classes: "class-1 class-2",
@@ -16,6 +18,9 @@ var tour = new Shepherd.Tour({
         block: "center"
       },
       showCancelLink: false,
+      popperOptions: {
+        modifiers: [{ name: 'offset', options: { offset: [0, 18] } }]
+      },
       tippyOptions: {
         maxWidth: "400px",
         popperOptions: {
@@ -30,7 +35,7 @@ var tour = new Shepherd.Tour({
     // classPrefix: prefix,
     // This should add the first tour step
     steps: [],
-    useModalOverlay: true,
+    useModalOverlay: false,
     styleVariables: {
       // arrowSize:2.5,
       shepherdThemePrimary: "#1b5486",
@@ -40,18 +45,176 @@ var tour = new Shepherd.Tour({
       // //   shepherdHeaderBackground: '#eeeeee',
       // //   shepherdThemePrimary: '#9b59b6',
       //   useDropShadow: true,
-      overlayOpacity: 0.25
-    }
+      overlayOpacity: 0.1
+    },
+    when: {
+        show() {
+          const currentStepElement = shepherd.currentStep.el;
+          const header = currentStepElement.querySelector('.shepherd-header');
+          const progress = document.createElement('span');
+          progress.style['margin-right'] = '315px';
+          progress.innerText = `${shepherd.steps.indexOf(shepherd.currentStep) + 1}/${shepherd.steps.length}`;
+          header.insertBefore(progress, currentStepElement.querySelector('.shepherd-cancel-icon'));        
+        }
+      }
   });
 
+function setUp(){
+    
+}
 let steps = [{
-    title: 'Creating a Shepherd Tour',
-    text: `Creating a Shepherd tour is easy. too!\
-    Just create a \`Tour\` instance, and add as many steps as you want.`,
+    title: 'Frequency of Symptoms',
+    text: `This chart shows the percentage of patients that reported common COVID symptoms.`,
     attachTo: {
-      element: '#FatigueBar',
-      on: 'left'
+        element: '.horizontal-bars',
+        on: 'right'
+      },
+    buttons: [
+    //   {
+    //     action() {
+    //       return this.back();
+    //     },
+    //     classes: 'shepherd-button-secondary',
+    //     text: 'Back'
+    //   },
+      {
+        action() {
+          return this.next();
+        },
+        text: 'Next'
+      },
+    ],
+    id: 's1'
+  },
+  {
+    title: 'Question',
+    text: `Select the bar representing people who reported cough as one of their symptoms.`,
+    attachTo: {
+        element: '.horizontal-bars',
+        on: 'bottom'
+      },
+    buttons: [
+      {
+        action() {
+          return this.back();
+        },
+        classes: 'shepherd-button-secondary',
+        text: 'Back'
+      },
+    //   {
+    //     action() {
+    //       return this.next();
+    //     },
+    //     text: 'Next'
+    //   },
+    ],
+    id: 's2'
+  },
+  {
+    title: 'Good Job!',
+    text: `These patients reported Fatigue and Anosmia as symptoms `,
+    attachTo: {
+        element: '.horizontal-bars',
+        on: 'bottom'
+      },
+    buttons: [
+      {
+        action() {
+          return this.back();
+        },
+        classes: 'shepherd-button-secondary',
+        text: 'Back'
+      },
+      {
+        action() {
+            revealCol('#CoughAnosmiaFatigueVBar','#b')
+            
+        //   return this.next();
+        },
+        text: 'Next'
+      },
+    ],
+    id: 's3'
+  },
+  {
+    // title: 'These patients reported Fatigue, Anosmia, and Cough as symptoms',
+    text: `These patients reported Fatigue, Anosmia, and Cough as symptoms`,
+    attachTo: {
+        element: '.horizontal-bars',
+        on: 'bottom'
+      },
+    buttons: [
+      {
+        action() {
+          return this.back();
+        },
+        classes: 'shepherd-button-secondary',
+        text: 'Back'
+      },
+      {
+        action() {
+            revealCol('#FatigueVBar','#c')
+        },
+        text: 'Next'
+      },
+    ],
+    id: 's4'
+  },
+  {
+    title: 'These patients *only* reported Fatigue as a symptom',
+    // text: `The top bars show... `,
+    attachTo: {
+      element: '.horizontal-bars',
+      on: 'bottom'
     },
+    buttons: [
+      {
+        action() {
+          return this.back();
+        },
+        classes: 'shepherd-button-secondary',
+        text: 'Back'
+      },
+      {
+        action() {
+            revealAllCols()
+        },
+        text: 'Next'
+      },
+    ],
+    id: 's5'
+  },
+  {
+    title: 'Question',
+    text: `Select the bar representing people who reported cough as their ONLY symptom.`,
+    attachTo: {
+        element: '.setCircles',
+        on: 'bottom'
+      },
+    buttons: [
+      {
+        action() {
+          return this.back();
+        },
+        classes: 'shepherd-button-secondary',
+        text: 'Back'
+      },
+    //   {
+    //     action() {
+    //       return this.next();
+    //     },
+    //     text: 'Next'
+    //   },
+    ],
+    id: 's6'
+  },
+  {
+    title: 'Great Job! You are done!',
+    // text: `Select the bar representing people who reported cough as their ONLY symptom.`,
+    attachTo: {
+        element: '.setCircles',
+        on: 'bottom'
+      },
     buttons: [
       {
         action() {
@@ -64,10 +227,10 @@ let steps = [{
         action() {
           return this.next();
         },
-        text: 'Next'
-      }
+        text: 'Exit'
+      },
     ],
-    id: 'creating'
+    id: 's6'
   }]
 
 
