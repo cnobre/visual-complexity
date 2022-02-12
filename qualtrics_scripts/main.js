79cd421620b63073e80daff562b5c09fc74edd2d
Carolina <carolinanobre@gmail.com>
2022-02-11 22:56:09 -0500

    updating test scripts

--- qualtrics_scripts/main.js
+++ qualtrics_scripts/main.js
@@ -1,11 +1,25 @@
-function interact(data){
-    console.log('data is ', data);
+function interact(this){
+
+    	
+	var inputId = 'QR~' + this.questionId;
+    console.log('inputId is', inputId)
+	
+	d3.xml("https://raw.githubusercontent.com/cnobre/visual-complexity/barbara/hotel_costs.svg")
+		.then((data) => {
+    // do something with the data
 		
-    d3.select('#myImage').node().append(data.documentElement);
-    
-    d3.select('svg')
-        .attr('width', '600px')
-        .attr('height', '200px');
-    
-    d3.select('#vodka_ny').on('click', function(){d3.select(this).style('fill', 'black')})
+        d3.select('#myImage').node().append(data.documentElement);
+        
+        d3.select('svg')
+            .attr('width', '600px')
+            .attr('height', '200px');
+        
+        d3.select('#vodka_ny').on('click', function(){d3.select(this).style('fill', 'black')})
+  })
+  .catch((error) => {
+    console.error("Error loading the data", error);
+  });
+
+
+   
 }
